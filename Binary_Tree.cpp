a32c6446c7f919f67782a4b4be6f472a1189b30c
Sagar Solanki <139437911+GeorgianSagar@users.noreply.github.com>
2025-09-09 12:18:10 +0000

    Level order traversal of a binary tree

--- Binary_Tree.cpp
+++ Binary_Tree.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<vector>
+#include<queue>
 
 using namespace std;
 
@@ -56,6 +57,30 @@ void postOrder(Node* root){
     postOrder(root->right);
     cout<<root->data<<" ";
 }
+//Level Order Traversal / BFS
+void levelOrder(Node* root){
+    if(root == NULL){
+        return;
+    }
+    queue<Node*> q;
+    q.push(root);
+
+
+    while(q.size() > 0){
+        Node* curr = q.front();
+        q.pop();
+
+        cout<<curr->data<<" ";
+
+        if(curr->left != NULL){
+            q.push(curr->left);
+        }
+        if(curr->right != NULL){
+            q.push(curr->right);
+        }
+    }
+    cout<<endl;
+}
 
 int main(){
     vector<int> preorder = {1,2,-1,-1,3,4,-1,-1,5,-1,-1};
@@ -74,6 +99,8 @@ int main(){
 
     postOrder(root); // 2 4 5 3 1
     cout<<endl;
+
+    levelOrder(root); // 1 2 3 4 5
 
     return 0;
 }
